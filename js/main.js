398501cc67c7aee67538e95fec1df0c6106c8441
João Oliveira <joaoo.rocha1@gmail.com>
2021-01-23 11:48:53 -0300

    Adição da project area, say about me,contact me, footer e responsividade do site

--- js/main.js
+++ js/main.js
@@ -23,4 +23,39 @@ $(document).ready(function(){
         gallery: { enabled: true }
     });
 
+    //Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height() + 10;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
 });
